933c7f2e0e91525bfae734002fd7ac54599b582d
blarrise <61951559+blariise@users.noreply.github.com>
2025-05-01 22:50:38 +0200

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (!(Number.isInteger(start) && Number.isInteger(end))
+     || (start < 1 || end < 1)
+     || (start % 1 !== 0 || end % 1 !== 0)) {
+    return "ERROR";
+  }
 
+  if (start > end) {
+    let temp = end;
+    end = start;
+    start = temp;
+  }
+  let sum = 0;
+  while(start <= end) {
+    sum += start;
+    ++start;
+  }
+  return sum;
 };
 
 // Do not edit below this line
