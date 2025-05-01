d4fc2e338d4ecceac1ae02536832bbb4e7f1a4c6
blarrise <61951559+blariise@users.noreply.github.com>
2025-05-01 19:22:27 +0200

    Add whole game functionality

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
 
@@ -13,5 +16,48 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  return prompt("Choose rock, paper or scissors!");
+  return prompt("Choose rock, paper or scissors!").toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("Tie");
+    return;
+  }
+
+  let isHumanWinner = false;
+
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    isHumanWinner = true;
+  }
+  if (humanChoice === "paper" && computerChoice === "rock") {
+    isHumanWinner = true;
+  }
+  if (humanChoice === "scissors" && computerChoice === "paper") {
+    isHumanWinner = true;
+  }
+  
+  let message = null;
+
+  if (isHumanWinner) {
+    ++humanScore;
+    message = `You win! ${humanChoice} beats ${computerChoice}`;
+  } else {
+    ++computerScore;
+    message = `You lose! ${computerChoice} beats ${humanChoice}`;
+  }
+  console.log(message);
+}
+
+function playGame() {
+  for (let i = 0; i < 5; ++i) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+  if (humanScore > computerScore) {
+    console.log("Congratulation, You won!");
+  } else if ( humanScore < computerScore) {
+    console.log("Unlucky, You lost!");
+  } else {
+    console.log("Its tie");
+  }
 }
