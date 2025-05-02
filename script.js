d8b1aa0905c06b79e3322eaf73f70f0aa328b081
blarrise <61951559+blariise@users.noreply.github.com>
2025-05-02 18:51:17 +0200

    Add hover effect when pressing down mouse button

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const container = document.querySelector(".container");
+let isMouseDown = false;
 
 function createGrid() {
   for (let i = 0; i < 16 * 16; ++i) { 
     let square = document.createElement("div");
     square.setAttribute("class", "square");
+    square.setAttribute("id", `${i}`);
     container.appendChild(square);
   }
 }
-
-
 createGrid();
+
+document.addEventListener("mousedown", (e) => {
+  isMouseDown = true;
+});
+
+document.addEventListener("mouseup", () => {
+  isMouseDown = false;
+});
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+  square.addEventListener("mouseover", () => {
+    if (isMouseDown) {
+      square.style["background-color"] = "blue";
+      console.log(square.id);
+    }
+  });
+});
+
+
