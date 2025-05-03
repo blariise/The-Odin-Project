4662dd80b5af06481ce7a26c629bda87553d5dc5
blarrise <61951559+blariise@users.noreply.github.com>
2025-05-03 16:22:59 +0200

    09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+  const regex = /[a-z0-9]/g;
+  const filtered = palindrome.toLowerCase().match(regex).join("");
+  const rev = filtered.split("").reverse().join("");
+  return filtered === rev;
 };
 
 // Do not edit below this line
