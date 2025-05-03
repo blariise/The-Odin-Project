f784e61fc8c599cc65a9a503fb3fb42fd3832ed7
blarrise <61951559+blariise@users.noreply.github.com>
2025-05-03 16:30:37 +0200

    10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(target) {
+  if (target < 0)
+    return "OOPS";
+  if(target == 0)
+    return 0;
+  if (target == 1)
+    return 1;
+  return fibonacci(target - 1) + fibonacci(target - 2);
 };
 
 // Do not edit below this line
