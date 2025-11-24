6aec2ba5a7425055bb7dfeddec412e3d16b32b11
blarrise <61951559+blariise@users.noreply.github.com>
2025-11-24 17:59:18 +0100

    Add gameboard class and functionality

---
+++ src/gameboard.js
@@ -0,0 +1,98 @@
+export default class Gameboard {
+  width;
+  height;
+  board;
+
+  constructor(width = 10, height = 10) {
+    this.width = width;
+    this.height = height;
+    this.board = this.#initBoard();
+  }
+
+  #initBoard() {
+    let board = new Array(this.height);
+    for (let x = 0; x < this.height; ++x) {
+      board[x] = new Array(this.width);
+      for (let y = 0; y < this.width; ++y) {
+        board[x][y] = new Cell(0, null);
+      }
+    }
+    return board;
+  }
+
+  addShip(ship, x, y, direction) {
+    if (this.#checkCordinates()) {
+      switch(direction) {
+        case "vertical":
+          this.#addShipVertical(ship, x, y);
+          break;
+        case "horizontal":
+          this.#addShipHorizontal(ship, x, y);
+          break;
+        defualt:
+          throw new Error("Bad direction");
+      }
+    }
+  }
+
+  #addShipVertical(ship, x, y) {
+    const end_cell = ship.length + y - 1;
+    if (end_cell >= this.height) {
+      throw new Error("Ship out of bound");
+    }
+    for (let i = y; i <= end_cell; ++i) {
+      const cell = this.board[x][i];
+      if (cell.value == 1) {
+        throw new Error(`Ship already placed at (${x},${i})`);
+      }
+      cell.value = 1;
+      cell.owner = ship;
+    }
+  }
+
+  #addShipHorizontal(ship, x, y) {
+    const end_cell = ship.length + x - 1;
+    if (end_cell >= this.width) {
+      throw new Error("Ship out of bound");
+    }
+    for (let i = x; i <= end_cell; ++i) {
+      const cell = this.board[i][y];
+      if (cell.value == 1) {
+        throw new Error(`Ship already placed at (${i},${y})`);
+      }
+      cell.value = 1;
+      cell.owner = ship;
+    }
+  }
+
+  receiveAttack(x, y) {
+    if (this.#checkCordinates()) {
+      const cell = this.board[x][y];
+      if (cell.value) {
+        cell.value = -1;
+        cell.owner.hit();
+        console.log(cell.owner);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  #checkCordinates(x, y) {
+    if ((x < 0 || x >= this.width) && (y < 0 || y >= this.height)) {
+      throw new Error("Cordinates are out of bound");
+    }
+    return true;
+  }
+}
+
+class Cell {
+  value;
+  owner; // Ship, if cell is without a ship then null
+
+  constructor(value, owner) {
+    this.value = value;
+    this.owner = owner;
+  }
+}
+
