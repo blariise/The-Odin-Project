c1b2647c75592b86d5445b342f49c6b940c84fd2
blarrise <61951559+blariise@users.noreply.github.com>
2025-11-25 23:33:07 +0100

    Change test to refactored gameboard

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -12,26 +12,26 @@ describe("Gameboard class", () => {
     const gameboard = new Gameboard();
     const carrier = new Ship(5);
     gameboard.addShip(carrier, 0, 0, "vertical");
-    expect(gameboard.board[0][0].value).toBe(1);
-    expect(gameboard.board[0][1].value).toBe(1);
-    expect(gameboard.board[0][2].value).toBe(1);
-    expect(gameboard.board[0][3].value).toBe(1);
-    expect(gameboard.board[0][4].value).toBe(1);
+    expect(gameboard.board[0][0].value).toBe("ship");
+    expect(gameboard.board[0][1].value).toBe("ship");
+    expect(gameboard.board[0][2].value).toBe("ship");
+    expect(gameboard.board[0][3].value).toBe("ship");
+    expect(gameboard.board[0][4].value).toBe("ship");
 
     const battleship = new Ship(4);
     gameboard.addShip(battleship, 2, 6, "horizontal");
-    expect(gameboard.board[2][6].value).toBe(1);
-    expect(gameboard.board[3][6].value).toBe(1);
-    expect(gameboard.board[4][6].value).toBe(1);
-    expect(gameboard.board[5][6].value).toBe(1);
+    expect(gameboard.board[2][6].value).toBe("ship");
+    expect(gameboard.board[3][6].value).toBe("ship");
+    expect(gameboard.board[4][6].value).toBe("ship");
+    expect(gameboard.board[5][6].value).toBe("ship");
     
     expect(() => {
       gameboard.addShip(battleship, 7, 8, "horizontal");
-    }).toThrow("Ship out of bound");
+    }).toThrow("Cordinates are out of bound");
 
     expect(() => {
       gameboard.addShip(battleship, 0, 2, "horizontal");
-    }).toThrow('Ship already placed at');
+    }).toThrow("Ship already placed at");
   });
 
   test("receiveAttack()", () => {
@@ -39,7 +39,9 @@ describe("Gameboard class", () => {
     const carrier = new Ship(5);
     gameboard.addShip(carrier, 0, 0, "vertical");
     expect(gameboard.receiveAttack(0, 0)).toBeTruthy();
-    expect(gameboard.receiveAttack(1, 0)).toBeFalsy();
+    expect(gameboard.receiveAttack(1, 0)).toBeTruthy();
+    expect(gameboard.board[0][0].value).toBe("hit");
+    expect(gameboard.board[1][0].value).toBe("miss");
   });
 });
 
