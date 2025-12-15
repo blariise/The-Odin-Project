42bf20652052870f7ad75d69514a02af6508bd68
blarrise <61951559+blariise@users.noreply.github.com>
2025-12-15 19:10:19 +0100

    Add characters fetching function

---
+++ src/components/Data.js
@@ -0,0 +1,17 @@
+const apiUrl = "https://api.jikan.moe/v4/";
+
+//https://myanimelist.net/anime/1735/Naruto__Shippuuden/characters
+const chsId = [13, 17, 85, 145, 14, 2007, 1662, 1555, 2535, 2792, 3179, 2767, 20013];
+
+export default async function fetchCharacters(animeId = 1735, charactersId = chsId) {
+  const charactersUrl = `${apiUrl}anime/${animeId}/characters`;
+  const response = await fetch(charactersUrl);
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  const characters = data.data;
+  return characters.filter((item) => charactersId.includes(item.character.mal_id));
+}
+
