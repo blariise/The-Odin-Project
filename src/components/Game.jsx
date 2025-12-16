e2ba35c6516c05cf6f70965740b98b9ca553efe4
blarrise <61951559+blariise@users.noreply.github.com>
2025-12-16 05:50:09 +0100

    Add game functions

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -8,9 +8,53 @@ export function Game({children}) {
   const [characters, setCharacters] = useState(initCharacters);
   const [bestScore, setBestScore] = useState(0);
   const [currentScore, setCurrentScore] = useState(0);
+  const [clickedIds, setClickedIds] = useState([]);
+
+  let isGameOver = false;
 
   if (!characters) return null;
-  console.log(characters);
+
+  function handleClick(characterId) {
+    if (clickedIds.includes(characterId)) {
+      restartGame();
+      return;
+    }
+    setCurrentScore(currentScore + 1);
+    addClickedId(characterId);
+    setCharacters(getShuffledArray(characters));
+  }
+
+  function restartGame() {
+    setBestScore(currentScore);
+    setCurrentScore(0);
+    setClickedIds([]);
+    setCharacters(getShuffledArray(characters));
+  }
+
+  function addClickedId(characterId) {
+    setClickedIds([...clickedIds, characterId]);
+  }
+
+  function checkIfGameOver() {
+    if (currentScore === characters.length) {
+      console.log("Gameover");
+      isGameOver = true;
+    }
+  }
+
+  function getShuffledArray(arr) {
+    let arrCopy = [...arr];
+    if (arr.length < 2) return arrCopy;
+    for (let i = 0; i < arr.length; ++i) {
+      let randomIndex = Math.floor(Math.random() * (arr.length - 1));
+      let temp = arrCopy[i];
+      arrCopy[i] = arrCopy[randomIndex];
+      arrCopy[randomIndex] = temp;
+    }
+    return arrCopy;
+  }
+
+  checkIfGameOver();
 
   return (
     <div className={"game-container"}>
@@ -27,31 +71,43 @@ export function Game({children}) {
           </div>
         </div>
       </div>
-      <CardsContainer characters={characters}/>
+      <CardsContainer
+        characters={characters}
+        handleClick={handleClick}
+        isGameOver={isGameOver}
+      />
+      { 
+        isGameOver && 
+          <div className={"game-over"} onClick={restartGame}>
+            <p>Congratulation, You won!</p>
+          </div>
+      }
     </div>
   );
 }
 
-function CardsContainer({ characters }) {
+function CardsContainer({ characters, handleClick, isGameOver }) {
+  const classN = isGameOver ? "cards hide" : "cards";
   return (
-    <div className={"cards"}>
+    <div className={classN}>
       {
         characters.map((item) => {
           return (
             <CharacterCard 
               key={item.character.mal_id} 
               character={item.character}
+              handleClick={handleClick}
             />
           )
         })
       }
     </div>
-  )
+  );
 }
 
-function CharacterCard({ character }) {
+function CharacterCard({ character, handleClick}) {
   return (
-    <div className={"card"}>
+    <div className={"card"} onClick={() => handleClick(character.mal_id)}>
       <div className={"character-image"}>
         <img src={character.images.jpg.image_url} alt={character.name}/>
       </div>
